bdf27b54b024f045856068149f97678ea892eb6b
Christopher Orr <chris@orr.me.uk>
2017-01-17 01:22:55 +0100

    Add Pipeline implementation of test case, currently disabled.

---
+++ src/test/java/org/jenkinsci/plugins/gittagmessage/GitTagMessageExtensionPipelineTest.java
@@ -0,0 +1,46 @@
+package org.jenkinsci.plugins.gittagmessage;
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Ignore;
+
+import static org.jenkinsci.plugins.gittagmessage.GitTagMessageAction.ENV_VAR_NAME_MESSAGE;
+import static org.jenkinsci.plugins.gittagmessage.GitTagMessageAction.ENV_VAR_NAME_TAG;
+
+@Ignore("Disabled, as the extension does not work in Pipeline builds")
+public class GitTagMessageExtensionPipelineTest extends AbstractGitTagMessageExtensionTest<WorkflowJob, WorkflowRun> {
+
+    /**
+     * @param refSpec The refspec to check out.
+     * @param branchSpec The branch spec to build.
+     * @return A job configured with the test Git repo, given settings, and the Git Tag Message extension.
+     */
+    protected WorkflowJob configureGitTagMessageJob(String refSpec, String branchSpec) throws Exception {
+        String gitStep = String.format(
+            "checkout([$class: 'GitSCM', "
+                + "userRemoteConfigs: [[url: '%s', refspec: '%s']], "
+                + "branches: [[name: '%s']], "
+                + "extensions: [[$class: 'GitTagMessageExtension']]"
+                + "])",
+            repoDir.getRoot().getAbsolutePath(), refSpec, branchSpec);
+
+        WorkflowJob job = jenkins.createProject(WorkflowJob.class, "j");
+        job.setDefinition(new CpsFlowDefinition(""
+                + "node() {"
+                +    gitStep + "\n"
+                + "  echo \"tag='${env." + ENV_VAR_NAME_TAG + " }'\"\n"
+                + "  echo \"msg='${env." + ENV_VAR_NAME_MESSAGE + "}'\"\n"
+                + "}", true));
+        return job;
+    }
+
+    /** Asserts that the most recent build of the given job exported a tag message, or not exported if {@code null}. */
+    protected void assertBuildEnvironment(WorkflowRun build, String expectedName, String expectedMessage)
+            throws Exception {
+        // In Pipeline, unknown environment variables are returned as null
+        jenkins.assertLogContains(String.format("tag='%s'", expectedName), build);
+        jenkins.assertLogContains(String.format("msg='%s'", expectedMessage), build);
+    }
+
+}
